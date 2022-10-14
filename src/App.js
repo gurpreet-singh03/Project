b9b5a6ca5fb41b2e938a3973565397deaefe2a79
gurpreet-singh03 <gurpreetsingh03.2001@gmail.com>
2022-10-14 22:23:06 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+import Form from './Form';
+// import  {Form} from './Form.js';
+
+function App() {
+  return(
+  <>
+      <Form></Form>
+    </>
+  );
+}
+
+export default App;
